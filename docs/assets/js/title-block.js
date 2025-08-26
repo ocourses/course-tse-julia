14361e46f3fff01597a48fb9cd593e6b2925970a
Olivier Cots <olivier.cots@irit.fr>
2025-08-26 16:54:43 +0200

    fix index

--- docs/assets/js/title-block.js
+++ docs/assets/js/title-block.js
@@ -1,13 +1,41 @@
-var body_padding_top;
+
+function headerOffset() {
+// Set an offset if there is are fixed top navbar
+const headerEl = window.document.querySelector("header.fixed-top");
+if (headerEl) {
+    return headerEl.clientHeight;
+} else {
+    return 0;
+}
+}
+
+function footerOffset() {
+const footerEl = window.document.querySelector("footer.footer");
+if (footerEl) {
+    return footerEl.clientHeight;
+} else {
+    return 0;
+}
+}
+
+function dashboardOffset() {
+const dashboardNavEl = window.document.getElementById(
+    "quarto-dashboard-header"
+);
+if (dashboardNavEl !== null) {
+    return dashboardNavEl.clientHeight;
+} else {
+    return 0;
+}
+}
 
 window.onload = function get_body() {
-    var body = document.body;
-    body_padding_top = parseInt(window.getComputedStyle(body).getPropertyValue('padding-top'), 10);
     appHeight()
 }
 
 const appHeight = () => {
-    const vh = window.innerHeight - body_padding_top;
+    var bodyOffset = headerOffset() + footerOffset() + dashboardOffset();
+    const vh = window.innerHeight - bodyOffset; 
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
